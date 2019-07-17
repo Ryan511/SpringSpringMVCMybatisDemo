8584abd4c0249038d0b76bf39c497f0ed7f1fbe2
Ryan <Ryanchung511@foxmail.com>
2019-07-17 15:19:37 +0800

    2019071701 分离数据库配置

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -2,13 +2,18 @@ package com.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.demo.entity.User;
 import com.demo.service.UserService;
@@ -27,11 +32,19 @@ public class UserController {
 	public ModelAndView service(HttpServletRequest request, HttpServletResponse response) {
 		ModelAndView mv = new ModelAndView("hello");
 		List<User> users = userService.getUsers();
+		ServletContext application = ContextLoader.getCurrentWebApplicationContext().getServletContext();
+		//这个是获取Spring父上下文
+		ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(application);
+		//这个是获取子上下文 每个DispatcherServlet有一个自己的上下文对象 可访问父上下文
+		ApplicationContext  subContext = (ApplicationContext) RequestContextUtils.getWebApplicationContext(request);
+		String[] webString = context.getBeanDefinitionNames();
+		String[] subString = subContext.getBeanDefinitionNames();
+		String[] subToParent = subContext.getParent().getBeanDefinitionNames();
 		User vo = new User();
-		for(User po :users) {
-			vo =po;
+		for (User po : users) {
+			vo = po;
 		}
-		mv.addObject("user",vo);
+		mv.addObject("user", vo);
 		return mv;
 
 	}
